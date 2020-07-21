myenvis/EnVis

commit 1daa71d56e504bab3d212da08de0a4a9e4bccb87

Verschneidung mit Gelände
Die Version hat einen Bug beim Schneiden der Flächen. Noch nicht
ganz klar, ob das ein Problem in FreeCAD ist oder wir das API
missbrauchen.

Außerdem Grundgerüst, um Coverings zu verwalten.

EnVisHelper.py
@@ -17,6 +17,53 @@ def get_distance_vector(base_shape, target_shape):
             raise RuntimeError("Solutions don't match")
     return d
 
+def all_vertices_inside(vertices, shape):
+    for v in vertices:
+        if not shape.isInside(v.Point, 0.0, True):
+            return False
+    return True
+
+def get_opposite_face(shape, faceidx):
+    """Return the opposite face of shape"""
+    n = shape.Faces[faceidx].normalAt(0,0)
+    i = 0
+    while n.getAngle(-shape.Faces[i].normalAt(0,0)) > 0.001:
+        i += 1
+    return i
+
+def get_aligned_face(shape, face):
+    """Return (index, offset_vector) of the closest face of shape, that has the same (anti)normal
+
+    TODO: If there are multiple coplanar faces, a random one is selected, not the closest"""
+    n = face.normalAt(0,0)
+    p = face.findPlane()
+    i = 0
+    best = 2e100
+    for f in shape.Faces:
+        dot = n.dot(f.normalAt(0,0))
+        if dot < -0.99 or dot > 0.99:
+#            d = f.distToShape(face)[0] # maybe faster: (f2.valueAt(0,0) - f1.valueAt(0,0)).Length
+            d = p.projectPoint(f.Vertexes[0].Point, Method="LowerDistance")
+            if d < best:
+                best = d
+                best_i = i
+        i += 1
+    pos = shape.Faces[best_i].Surface.Position
+    return (best_i, p.projectPoint(pos) - pos)
+
+def make_intersection_candidate(shape, face, overlap=1):
+    """make_intersection_candidate(shape, face, overlap=1):
+    return a copy of shape translated along normals so that it might intersect face
+
+    overlap is in mm"""
+    (face_ind, d) = get_aligned_face(shape, face)
+    if d.Length == 0.0:
+        return shape
+    d.multiply(1 + overlap/d.Length)
+    test_shape = shape.copy()
+    test_shape.translate(d)
+    return test_shape
+
 def snap_by_resize_Zlength(shape, target):
     replacements = []
     candidates = sorted(shape.OuterWire.Edges, key=lambda e: e.BoundBox.ZLength)[-2:]

EnVisBruttoFaces.py
@@ -6,10 +6,13 @@ import EnVisOuterSpace
 
 class EnVisBruttoFace:
     def __init__(self, obj):
-        self.children = []  # Objects related to this face
+        self.full_covers = []  # Objects related to this face
+        self.partial_covers = []
         obj.addProperty('App::PropertyLinkSub', 'BaseFace', 'Envis', '')
         obj.addProperty('App::PropertyLink', 'Space', 'Envis', '')
+        obj.addProperty("App::PropertyLinkList",'CoversSpace', 'Envis', 'Bekleidungen auf der Raumseite')
         obj.addProperty('App::PropertyLink', 'Space2', 'Envis', '')
+        obj.addProperty("App::PropertyLinkList",'CoversSpace2', 'Envis', 'Bekleidungen auf der Außenseite bzw. Raum2')
         obj.addProperty('App::PropertyLink', 'SpaceBoundary', 'Envis', '')
         obj.Proxy = self
         if obj.ViewObject:
@@ -40,6 +43,43 @@ def makeBruttoFace(space_boundary, other_space, BaseFace=None, doc=None):
 
     return obj
 
+def copyBruttoFace(orig):
+    obj = makeBruttoFace(orig.SpaceBoundary, orig.Space2, BaseFace=orig.BaseFace, doc=orig.Document)
+    obj.Proxy.full_covers = orig.Proxy.full_covers.copy()
+    obj.Proxy.partial_covers = orig.Proxy.partial_covers.copy()
+
+    return obj
+
+def setup_coverings(bf):
+    """Check for full/partial covers"""
+
+    def nameify(obj):
+        if type(obj) == str:
+            return str
+        return obj.Name
+
+    doc = bf.Document
+    full_covers = {nameify(o) for o in bf.Proxy.full_covers}
+    partial_covers = set(bf.Proxy.partial_covers)
+    bf.Proxy.partial_covers = []
+    to_delete = []
+
+    for c in partial_covers:
+        if type(c) == str:
+            c = doc.getObject(c)
+        test_shape = make_intersection_candidate(c.Shape, bf.Shape)
+        f = bf.Shape.common(test_shape)
+        if f.Area < 100:  # mm^2
+            to_delete.append(c)
+        elif f.Area > bf.Shape.Area - 100:
+            full_covers.add(c.Name)
+        else:
+            bf.Proxy.partial_covers.append(c.Name)
+
+    bf.Proxy.full_covers = full_covers
+    bf.CoversSpace = list(filter(lambda o: o not in to_delete, bf.CoversSpace))
+    bf.CoversSpace2 = list(filter(lambda o: o not in to_delete, bf.CoversSpace2))
+
 def innerOuter(sbs):
     i = set()
     e = set()
@@ -63,14 +103,6 @@ def pop_pair(sbs):
     sbs.remove(other)
     
     return sb, other
-
-def get_opposite(shape, faceidx):
-    """Return the opposite face of shape"""
-    n = shape.Faces[faceidx].normalAt(0,0)
-    i = 0
-    while n.getAngle(-shape.Faces[i].normalAt(0,0)) > 0.001:
-        i += 1
-    return i
 
 def faceFromLinkSub(prop):
     """returns tuple id, faceidx"""
@@ -104,9 +136,9 @@ def createModel(layer):
     def handle_external_case(sb):
         """Return a BruttoFace for SpaceBoundary or None, if it shoule be dropped
 
-        Not implemented: The BruttoFace is linked to the proper Außenraum"""
+        The BruttoFace is linked to the proper Außenraum"""
         obj, faceind = faceFromLinkSub(sb.BaseFace)
-        outer_face_ind = get_opposite(obj.Shape, faceind)
+        outer_face_ind = EnVisHelper.get_opposite_face(obj.Shape, faceind)
         outer_face = obj.Shape.Faces[outer_face_ind]
         d = EnVisHelper.get_distance_vector(obj.Shape.Faces[faceind], outer_face)
         offset = d.add(outer_face.normalAt(0,0).multiply(project.intersectionTolerance))
@@ -116,14 +148,19 @@ def createModel(layer):
         bbox.enlarge(project.intersectionTolerance)
         candidates = list(filter(lambda o: o != obj and bbox.intersect(o.Shape.BoundBox), building_objs))
         print("Found", len(candidates), "intersection candidates")
-        coverings = []
+        coverings_full = []
+        coverings_partial = []
+        outer_space = None
         for o in candidates:
             cover = offset_sb.common(o.Shape)
             if cover.Faces:
-                if o.IfcType == "Covering":  # TODO recursive offset/intersection
-                    coverings.append(o)
-                    continue
                 f = cover.Faces[0]
+                if o.IfcType == "Covering":  # TODO recursive offset/intersection
+                    if EnVisHelper.isClose(f.Area, offset_sb.Area):
+                        coverings_full.append(o)
+                    else:
+                        coverings_partial.append(o)
+                    continue
                 print("candiate", o.Name, "has area ratio", f.Area/offset_sb.Area)
                 if isBuildingObj(o):
                     print("Dropping")
@@ -132,13 +169,19 @@ def createModel(layer):
                     outer_space = EnVisOuterSpace.get_outer_space(o)
                     if EnVisHelper.isClose(f.Area, offset_sb.Area):
                         break
-                    # TODO add outer_space to children
-        outer_space = EnVisOuterSpace.get_outer_space(math.degrees(FreeCAD.Vector(0,0,1).getAngle(outer_face.normalAt(0,0))))
-        # TODO add coverings to children
+                    coverings_partial.insert(0, outer_space)
+                    outer_space = None
+        if not outer_space:
+            outer_space = EnVisOuterSpace.get_outer_space(math.degrees(FreeCAD.Vector(0,0,1).getAngle(outer_face.normalAt(0,0))))
         if project.moveOuterSB:
-            return makeBruttoFace(sb, outer_space, BaseFace=linkSubFromFace(obj, outer_face_ind))
+            bf = makeBruttoFace(sb, outer_space, BaseFace=linkSubFromFace(obj, outer_face_ind))
         else:
-            return makeBruttoFace(sb, outer_space)
+            bf = makeBruttoFace(sb, outer_space)
+        bf.CoversSpace2 = coverings_partial + coverings_full
+        bf.Proxy.partial_covers = coverings_partial
+        bf.Proxy.full_covers = coverings_full
+
+        return bf
 
     for sbs in byBE.values():
         beObj = sbs[0].BuildingElement
@@ -290,6 +333,23 @@ def createModel(layer):
                         dist = d
                         best = target
                 best.Shape = f
+
+    # final step: split the brutto faces, that have more then one outer space
+    # and also setup coverings
+    for bf in brutto_faces:
+        while bf.Proxy.partial_covers and type(bf.Proxy.partial_covers[0].Proxy) == EnVisOuterSpace.EnVisOuterSpace:
+            outer_space = bf.Proxy.partial_covers.pop(0)
+            test_shape = EnVisHelper.make_intersection_candidate(outer_space.BaseObject.Shape, bf.Shape, project.intersectionTolerance.Value)
+            new_face = bf.Shape.common(test_shape)
+            rest_face = bf.Shape.cut(test_shape)
+            new = copyBruttoFace(bf)
+            while new.Proxy.partial_covers and type(new.Proxy.partial_covers[0].Proxy) == EnVisOuterSpace.EnVisOuterSpace:
+                del new.Proxy.partial_covers[0]
+            new.Shape = new_face
+            new.Space2 = outer_space
+            setup_coverings(new)
+            bf.Shape = rest_face
+        setup_coverings(bf)
 
     doc.recompute()
 

commit 82258e124f8f72be097e85e037dda0fd3531e7a8

Fix face cutting bug
Fixes: 1daa71d56e504bab3d212da08de0a4a9e4bccb87

EnVisHelper.py
@@ -70,7 +70,9 @@ def snap_by_resize_Zlength(shape, target):
     for e in candidates:
         d = get_distance_vector(e, target.Shape.OuterWire)
         if d.Length < 500 and d.Length > 0.0: # TODO: Add auto-detection
-            replacements.extend(zip(e.Vertexes, e.translated(d).Vertexes))
+            e_new = e.translated(d)
+            replacements.append((e, e_new))
+            replacements.extend(zip(e.Vertexes, e_new.Vertexes))
     if replacements:
         new_shape = shape.replaceShape(replacements)
         if not new_shape.isValid():